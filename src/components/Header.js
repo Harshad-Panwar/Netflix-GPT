6a78977161ea6fe1374bef6e8861abd41074adfb
Harshad Panwar <harshadpawar24@gmail.com>
2024-06-09 14:38:10 +0530

    Added App Favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,28 @@
 import React from "react";
-import logo from "../utils/Netflix_Logo_PMS.png"
+import logo from "../utils/Netflix_Logo_PMS.png";
 
-const Header = ({setSignUp}) => {
+const Header = ({ setSignUp }) => {
   return (
-      <div className=" bg-gradient-to-b from-black w-full absolute flex justify-around items-center pt-2 z-20">
-        <img
-          className=" w-44"
-          src={logo}
-          alt="logo"
-        />
-        <div className="flex gap-6">
-          <button className=" w-20 bg-[#FA0100] p-2 text-white text-sm font-bold rounded-lg" onClick={()=>setSignUp(false)}>
-            Sign In
-          </button>
-          <button className=" w-20 bg-[#FA0100] p-2 text-white text-sm font-bold rounded-lg" onClick={()=>setSignUp(true)} >
-            Sign Up
-          </button>
-        </div>
+    <div className=" bg-gradient-to-b from-black w-full absolute flex justify-around items-center pt-2 z-20">
+      <div>
+        <h1 className="text-xl text-white absolute font-bold pt-9 px-[70px] opacity-90">GPT</h1>
+        <img className=" w-44" src={logo} alt="logo" />
       </div>
+      <div className="flex gap-6">
+        <button
+          className=" w-20 bg-[#FA0100] p-2 text-white text-sm font-bold rounded-lg"
+          onClick={() => setSignUp(false)}
+        >
+          Sign In
+        </button>
+        <button
+          className=" w-20 bg-[#FA0100] p-2 text-white text-sm font-bold rounded-lg"
+          onClick={() => setSignUp(true)}
+        >
+          Sign Up
+        </button>
+      </div>
+    </div>
   );
 };
 
